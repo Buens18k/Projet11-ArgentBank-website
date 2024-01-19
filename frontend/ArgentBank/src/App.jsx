f094e0f3b9d67eb08bc7da28f6710c911b2263b0
Bruno Vang <buens@outlook.fr>
2024-01-19 10:53:27 +0100

    Page/component Error404

--- frontend/ArgentBank/src/App.jsx
+++ frontend/ArgentBank/src/App.jsx
@@ -3,12 +3,13 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import './App.css';
 import Layout from './Layout/Layout';
+import Error404 from './pages/Error404';
 
 const App = createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
-
+    errorElement: <Error404 />,
   },
 ]);
 
---
+++ frontend/ArgentBank/src/components/ErrorMessage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { isRouteErrorResponse } from "react-router-dom";
+
+import Main from "./Main";
+
+
+export default function ErrorMessage({error}) {
+    if (isRouteErrorResponse(error)) {
+        return(
+            <Main>
+                <div>
+                    <h1>{error.status}</h1>
+                    <p>The request page does not exist</p>
+                </div>
+            </Main>
+        )
+    }
+}
