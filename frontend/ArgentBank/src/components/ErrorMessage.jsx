39be2faa388a5dda2724e444d45741ff7c35cf6c
Bruno Vang <buens@outlook.fr>
2024-01-21 22:28:27 +0100

    prettier

--- frontend/ArgentBank/src/components/ErrorMessage.jsx
+++ frontend/ArgentBank/src/components/ErrorMessage.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { isRouteErrorResponse } from "react-router-dom";
+import React from 'react';
+import { isRouteErrorResponse } from 'react-router-dom';
 
-import Main from "./Main";
+import Main from './Main';
 
-
-export default function ErrorMessage({error}) {
-    if (isRouteErrorResponse(error)) {
-        return(
-            <Main>
-                <div>
-                    <h1>{error.status}</h1>
-                    <p>The request page does not exist</p>
-                </div>
-            </Main>
-        )
-    }
+export default function ErrorMessage({ error }) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <Main>
+        <div>
+          <h1>{error.status}</h1>
+          <p>The request page does not exist</p>
+        </div>
+      </Main>
+    );
+  }
 }
